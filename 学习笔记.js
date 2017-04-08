19a9851c5ca0c86523624fb3f5f5033b39b749cf
Wumingjun1991 <499567890@qq.com>
2017-04-08 14:15:05 +0800

    CSS init

---
+++ 学习笔记.js
@@ -0,0 +1,26 @@
+/**
+ * Created by w4995 on 2017/3/14.
+ */
+一.webstorm安装
+1.下载webstorm （）
+2.安装
+3.破解（）
+4.配置
+
+新建md格式步骤
+1. file--seeting-aaaa-aaaa
+
+markdown格式语法学习
+（http://www.jianshu.com/p/1e402922ee32/）
+
+
+二 javascript视频学习笔记
+
+1.
+
+2.js引入到页面的方式和细节
+
+A.111111
+B.222222222
+C.5555555555
+
---
+++ 第一天/windows快捷键.md
@@ -0,0 +1,10 @@
+window+E 打开我的电脑
+Alt+Tab 切换操作界面
+window+R 运行
+ctrl+C 复制
+ctrl+A 全选
+ctrl+V 粘贴
+ctrl+X 剪切
+ctrl+Z 撤销
+ctrl+S 保存
+ctrl+shift+S 另存
